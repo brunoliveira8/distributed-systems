ec546adb908e8f2bcc6d6cbb223ffcf60f820732
Bruno Oliveira <tarcisiobrunooliveira@gmail.com>
2016-10-27 11:36:21 -0300

    Auto pep8

--- ClientStorage.py
+++ ClientStorage.py
@@ -1,11 +1,13 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 import os
 import Pyro4
 
+
 def main():
 
-	storage = Pyro4.Proxy("PYRONAME:storage.server-1")
-	print(storage.retrieve("name.txt"))
+    storage = Pyro4.Proxy("PYRONAME:storage.server-1")
+    print(storage.retrieve("name.txt"))
+
 
 if __name__ == '__main__':
-	main()
+    main()
--- StorageServer.py
+++ StorageServer.py
@@ -1,58 +1,56 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 import os
 import Pyro4
 
+
 @Pyro4.expose
 class StorageServer:
-	
-	n_count = 0
 
-	def __init__(self):
-		self.pk = StorageServer.n_count+1
-		self.db = "storage-{0}".format(self.pk)
-		try:
-			os.mkdir(self.db)
-		except:
-			pass
+    n_count = 0
 
-		StorageServer.n_count+=1
+    def __init__(self):
+        self.pk = StorageServer.n_count + 1
+        self.db = "storage-{0}".format(self.pk)
+        try:
+            os.mkdir(self.db)
+        except:
+            pass
 
-	def save(self, file, filename):
-		with open(os.path.join(self.db, filename), "w") as f:
-			f.write(file)
+        StorageServer.n_count += 1
 
+    def save(self, file, filename):
+        with open(os.path.join(self.db, filename), "w") as f:
+            f.write(file)
 
-	def retrieve(self, filename):
-		with open(os.path.join(self.db, filename), "r") as f:
-			data = f.read()
-			return data
+    def retrieve(self, filename):
+        with open(os.path.join(self.db, filename), "r") as f:
+            data = f.read()
+            return data
 
-
-	def list(self):
-		data = os.listdir(self.db)
-		return data
+    def list(self):
+        data = os.listdir(self.db)
+        return data
 
 
 def main():
-	
-	n_servers = 2
-	servers = list()
 
-	for _ in range(n_servers):
-		servers.append(StorageServer())
-	
+    n_servers = 2
+    servers = list()
 
-	with Pyro4.Daemon() as daemon:
-		with Pyro4.locateNS() as ns:
-			for server in servers:
-				uri = daemon.register(server)
-				ns.register("storage.server-{0}".format(server.pk), uri)
+    for _ in range(n_servers):
+        servers.append(StorageServer())
 
-		print("{} servers registered.".format(n_servers))
-		print("\n Running...")
+    with Pyro4.Daemon() as daemon:
+        with Pyro4.locateNS() as ns:
+            for server in servers:
+                uri = daemon.register(server)
+                ns.register("storage.server-{0}".format(server.pk), uri)
 
-		daemon.requestLoop()
+        print("{} servers registered.".format(n_servers))
+        print("\n Running...")
+
+        daemon.requestLoop()
 
 
 if __name__ == '__main__':
-	main()
+    main()
